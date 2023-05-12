c19c8a16388668ee8d6e4471e25a0ebab78f578f
baltermia <54800864+baltermia@users.noreply.github.com>
2023-05-12 14:53:25 +0200

    Allow Swagger to run in Release Mode

--- BookingHive.Api/Program.cs
+++ BookingHive.Api/Program.cs
@@ -16,11 +16,8 @@ public class Program
         WebApplication app = builder.Build();
 
         // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
+        app.UseSwagger();
+        app.UseSwaggerUI();
 
         app.UseHttpsRedirection();
 
