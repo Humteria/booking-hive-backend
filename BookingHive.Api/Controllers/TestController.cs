0d35b4d997bdff526da4cc4c8fd5901ca1879fea
baltermia <54800864+baltermia@users.noreply.github.com>
2023-05-12 14:36:08 +0200

    Create TestController

---
+++ BookingHive.Api/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookingHive.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public ActionResult<string> Index()
+        {
+            return "Works";
+        }
+    }
+}
