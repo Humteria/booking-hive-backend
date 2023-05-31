c949bc9109b633611feb96cc78e9aef39f0787e9
baltermia <54800864+baltermia@users.noreply.github.com>
2023-05-31 12:10:52 +0200

    Create ServiceConfiguration for db Service-table config

---
+++ src/Infrastructure/Persistence/Configurations/ServiceConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using BookingHive.Domain.Entities;
+using BookingHive.Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookingHive.Infrastructure.Persistence.Configurations;
+
+public class ServiceConfiguration : IEntityTypeConfiguration<Service>
+{
+    public void Configure(EntityTypeBuilder<Service> builder)
+    {
+        builder.Property(s => s.Title)
+               .HasMaxLength(100)
+               .IsUnicode()
+               .IsRequired();
+        
+        builder.Property(s => s.Description)
+               .IsUnicode()
+               .HasMaxLength(350);
+
+        builder.Property(s => s.State)
+               .HasDefaultValue(ServiceState.Active);
+    }
+}
