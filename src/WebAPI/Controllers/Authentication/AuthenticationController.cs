09b8aa5ba819fee7564483be2c945670769effc6
baltermia <54800864+baltermia@users.noreply.github.com>
2023-06-02 14:11:32 +0200

    Create Authentication-Controller in WebAPI project

---
+++ src/WebAPI/Controllers/Authentication/AuthenticationController.cs
@@ -0,0 +1,21 @@
+﻿using BookingHive.Application.Authentication;
+using BookingHive.Application.Authentication.Commands;
+using BookingHive.Application.Common.Models;
+using BookingHive.WebAPI.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers.Authentication;
+
+public class AuthenticationController : ApiControllerBase
+{
+    [HttpPost]
+    public async Task<ActionResult<AuthenticatedUserDto>> Authenticate(AuthenticateCommand command)
+    {
+        ValueResult<AuthenticatedUserDto> jwtResult = await Mediator.Send(command);
+
+        return 
+            jwtResult.Succeeded ?
+                Ok(jwtResult.Value) :
+                Unauthorized();
+    }
+}
