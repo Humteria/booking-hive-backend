3188b8433cecb4fad4e47267f1f4917746bb777c
baltermia <54800864+baltermia@users.noreply.github.com>
2023-06-02 10:09:55 +0200

    Use ValueResult in IIdentityService

--- src/Application/Common/Interfaces/IIdentityService.cs
+++ src/Application/Common/Interfaces/IIdentityService.cs
@@ -10,7 +10,7 @@ public interface IIdentityService
 
     Task<bool> AuthorizeAsync(string userId, string policyName);
 
-    Task<(Result Result, string UserId)> CreateUserAsync(string userName, string password);
+    Task<ValueResult<string>> CreateUserAsync(string username, string password);
 
     Task<Result> DeleteUserAsync(string userId);
 }
