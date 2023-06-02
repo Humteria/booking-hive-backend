5c1986933db24c64734575e6ed05e38330ff4878
baltermia <54800864+baltermia@users.noreply.github.com>
2023-06-02 10:04:27 +0200

    Add ValueResult to Identity-Extensions

--- src/Infrastructure/Identity/IdentityResultExtensions.cs
+++ src/Infrastructure/Identity/IdentityResultExtensions.cs
@@ -5,10 +5,13 @@ namespace BookingHive.Infrastructure.Identity;
 
 public static class IdentityResultExtensions
 {
-    public static Result ToApplicationResult(this IdentityResult result)
-    {
-        return result.Succeeded
+    public static Result ToApplicationResult(this IdentityResult result) =>
+        result.Succeeded
             ? Result.Success()
             : Result.Failure(result.Errors.Select(e => e.Description));
-    }
+
+    public static ValueResult<T> ToApplicationValueResult<T>(this IdentityResult result, T value) =>
+        result.Succeeded
+            ? ValueResult<T>.Success(value)
+            : new ValueResult<T>(Result.Failure(result.Errors.Select(e => e.Description)));
 }
