2c507552c6afb8dd07ef07ee6e603be9e1532359
baltermia <54800864+baltermia@users.noreply.github.com>
2023-06-02 14:08:48 +0200

    Add JwtSettings in WebAPI app-settings and configure Model

---
+++ src/Infrastructure/Identity/JwtSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace BookingHive.Infrastructure.Identity;
+
+public class JwtSettings
+{
+    public string? SigningKey { get; init; }
+
+    public string? Issuer { get; init; }
+
+    public string? Audience { get; init; }
+
+    public int ExpiryInMinutes { get; init; }
+
+    public TimeSpan Expiration => TimeSpan.FromMinutes(ExpiryInMinutes);
+}
